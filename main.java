dae6864307d3b69b6155f034e511b3a3fceb8fc6
Charan <charan17ilango@gmail.com>
2025-09-27 11:54:57 +0530

    23 to 26 programs

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class main{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+     System.out.println("The Factorial Number is :");
+     int num = 1;
+        for(int i=1;i<=5;i++){
+          fact += num;
+            System.out.println(fact);
+        }
+       }
+       }
+        
+        
+
+    
